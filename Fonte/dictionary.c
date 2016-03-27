d534741fd8268b517875be789d90654b78a76148
Gabriel Galli <g7.galli96@gmail.com>
2016-03-27 01:27:31 -0300

    Pequenos ajustes na identação e espaçamentos, nenhuma mudança significativa

--- Fonte/dictionary.c
+++ Fonte/dictionary.c
@@ -334,27 +334,20 @@ int procuraObjectArquivo(char *nomeTabela){
     return SUCCESS;
 }
 //
-struct fs_objects leObjeto(char *nTabela){
-
-    FILE *dicionario;
-    char *tupla = (char *)malloc(sizeof(char)*TAMANHO_NOME_TABELA);
-    memset(tupla, '\0', TAMANHO_NOME_TABELA);
+struct fs_objects leObjeto(char *nTabela) {
     int cod;
-
-    char directory[LEN_DB_NAME*2];
+    struct fs_objects objeto;
+    char directory[LEN_DB_NAME * 2];
+    FILE *dicionario = fopen(directory, "a+b"); // Abre o dicionario de dados
+    char *tupla = (char *) malloc(sizeof (char) * TAMANHO_NOME_TABELA);
+    memset(tupla, '\0', TAMANHO_NOME_TABELA);
     strcpy(directory, connected.db_directory);
     strcat(directory, "fs_object.dat");
 
-    dicionario = fopen(directory, "a+b"); // Abre o dicionario de dados.
-
-    struct fs_objects objeto;
-
-    if(!verificaNomeTabela(nTabela)){
+    if (!verificaNomeTabela(nTabela)) {
         printf("ERROR: relation \"%s\" was not found.\n", nTabela);
-        if (dicionario)
-            fclose(dicionario);
+        if (dicionario) fclose(dicionario);
         free(tupla);
-
         return objeto;
     }
 
@@ -364,19 +357,16 @@ struct fs_objects leObjeto(char *nTabela){
         return objeto;
     }
 
-
-    while(fgetc (dicionario) != EOF){
-        fseek(dicionario, -1, 1);
-
+    while (fgetc(dicionario) != EOF) {
+        fseek(dicionario, -1, SEEK_CUR);
         fread(tupla, sizeof(char), TAMANHO_NOME_TABELA , dicionario); //Lê somente o nome da tabela
-
-        if(objcmp(tupla, nTabela) == 0){ // Verifica se o nome dado pelo usuario existe no dicionario de dados.
-            strcpylower(objeto.nome, tupla);
-            fread(&cod,sizeof(int),1,dicionario);   // Copia valores referentes a tabela pesquisada para a estrutura.
-            objeto.cod=cod;
-            fread(tupla,sizeof(char),TAMANHO_NOME_TABELA,dicionario);
+        if (strcmp(tupla, nTabela) == 0) { // Verifica se o nome dado pelo usuario existe no dicionario de dados.
+            strcpy(objeto.nome, tupla);
+            fread(&cod, sizeof (int), 1, dicionario);   // Copia valores referentes a tabela pesquisada para a estrutura.
+            objeto.cod = cod;
+            fread(tupla, sizeof (char), TAMANHO_NOME_TABELA, dicionario);
             strcpylower(objeto.nArquivo, tupla);
-            fread(&cod,sizeof(int),1,dicionario);
+            fread(&cod, sizeof (int), 1, dicionario);
             objeto.qtdCampos = cod;
 
             free(tupla);
